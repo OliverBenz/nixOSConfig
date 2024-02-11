9ef253dbf91e06a7cc5e7c3bfa624cd5a75191b5
Oliver Benz <oliver.benz@outlook.com>
2024-02-11 15:34:36 +0100

    Cleanup users.

--- configuration.nix
+++ configuration.nix
@@ -10,55 +10,58 @@
   ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.oliver = {
-    isNormalUser = true;
-    description = "Oliver Benz";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = [
-      # Essential
-      pkgs.brave
-      pkgs.kate
-      pkgs.mpv
-      pkgs.gthumb
-      pkgs.flameshot
-      pkgs.libsForQt5.okular
-      pkgs.krita
+  users.users = {
+    oliver = {
+      isNormalUser = true;
+      description = "Oliver Benz";
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = [
+        # Essential
+        pkgs.brave
+        pkgs.kate
+        pkgs.mpv
+        pkgs.gthumb
+        pkgs.flameshot
+        pkgs.libsForQt5.okular
+        pkgs.krita
 
-      # Work
-      pkgs.slack
-      pkgs.whatsapp-for-linux
-      pkgs.bitwarden
-      pkgs-unstable.mathematica
-      pkgs.libreoffice
-      pkgs.thunderbird
-      pkgs.calibre
-      pkgs.onedrive
+        # Work
+        pkgs.slack
+        pkgs.whatsapp-for-linux
+        pkgs.bitwarden
+        pkgs-unstable.mathematica
+        pkgs.libreoffice
+        pkgs.thunderbird
+        pkgs.calibre
+        pkgs.onedrive
 
-      # Recreational
-      pkgs.discord
-      pkgs.obs-studio
-      pkgs.spotify
+        # Recreational
+        pkgs.discord
+        pkgs.obs-studio
+        pkgs.spotify
 
-      # Development
-      pkgs.jetbrains.clion
-      pkgs.vscode
-      pkgs.gcc13
-      pkgs.sqlitecpp
-      pkgs.cmake
-      pkgs.gnumake
-      pkgs.ninja
-      pkgs.sqlite
-      pkgs.qt6.full
-    ];
-  };
+        # Development
+        pkgs.jetbrains.clion
+        pkgs.vscode
+        pkgs.gcc13
+        pkgs.sqlitecpp
+        pkgs.cmake
+        pkgs.gnumake
+        pkgs.ninja
+        pkgs.sqlite
+        pkgs.qt6.full
+      ];
+    };
 
-  users.users.guest = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [
-      flameshot
-    ];
-    initialPassword = "guest";
+    guest = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      packages = with pkgs; [
+        flameshot
+      ];
+      initialPassword = "guest";
+    };
+    
   };
 
   # Allow unfree packages
